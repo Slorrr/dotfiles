4a27029ed31a28c7e7fe30be04f86ea7702a7da2
Slor <86879002+Slorrr@users.noreply.github.com>
2023-03-14 21:43:49 +0300

    upd nvim

---
+++ .config/nvim/lua/slor/init.lua
@@ -0,0 +1,3 @@
+require("slor.remap")
+require("slor.set")
+
---
+++ .config/nvim/lua/slor/set.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.incsearch = true
+
+vim.g.airline_extensions_tabline_enabled = 1
+
+
