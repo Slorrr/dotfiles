2aef330da83d9af50d1963d0b5250c1653235450
Slor <86879002+Slorrr@users.noreply.github.com>
2023-04-02 17:28:17 +0300

    Update remap.lua

--- nvim/lua/slor/remap.lua
+++ nvim/lua/slor/remap.lua
@@ -13,13 +13,29 @@ nnoremap <silent><C-h> <C-W><C-H>
 nnoremap <silent><C-l> <C-W><C-L>
 ]]
 
-vim.cmd[[ 
-nnoremap <silent><C-[> :BufferLineCyclePrev<CR>
-nnoremap <silent><C-]> :BufferLineCycleNext<CR>
-]]
+vim.keymap.set("n", "<leader>tb", function()
+    vim.cmd("TagbarToggle")
+end)
 
 vim.keymap.set("n", "<leader>v", function()
     vim.cmd("vsplit")
 end)
 
+vim.keymap.set("n", "<leader>bp", function()
+    vim.cmd(":BufferLinePick")
+end)
 
+vim.keymap.set("n", "<leader>bc", function()
+    vim.cmd(":BufferLinePickClose")
+end)
+
+vim.keymap.set("n", "<A-[>", function()
+   vim.cmd(":BufferLineCyclePrev")
+end)
+
+vim.keymap.set("n", "<A-]>", function()
+    vim.cmd(":BufferLineCycleNext")
+end)
+
+vim.keymap.set("n", "<esc>", function()
+end)
